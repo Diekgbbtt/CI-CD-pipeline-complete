63aea51bb038251260f8a343d441793d4ceff496
= <diegogobbetti111@gmail.com>
2025-01-04 23:35:31 +0100

    added first version jenkins file, jenkins documentation done

---
+++ src/test/java/com/diekgbbtt/devsecops/sampleapp/DemoappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diekgbbtt.devsecops.sampleapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
